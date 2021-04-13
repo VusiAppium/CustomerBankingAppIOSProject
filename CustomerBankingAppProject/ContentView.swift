859de7df054fc24a7eb566d9450a0da2c49de277
VusiAppium <vusi.ngwenya@absa.africa>
2021-04-13 13:36:36 +0200

    Initial Commit

---
+++ CustomerBankingAppProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomerBankingAppProject
+//
+//  Created by Vusi Ngwenya (ZA) on 2021/04/13.
+//  Copyright © 2021 Vusi Ngwenya (ZA). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomerBankingAppProjectTests/CustomerBankingAppProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  CustomerBankingAppProjectTests.swift
+//  CustomerBankingAppProjectTests
+//
+//  Created by Vusi Ngwenya (ZA) on 2021/04/13.
+//  Copyright © 2021 Vusi Ngwenya (ZA). All rights reserved.
+//
+
+import XCTest
+@testable import CustomerBankingAppProject
+
+class CustomerBankingAppProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
